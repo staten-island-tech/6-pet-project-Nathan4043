7564f06c668d4ddc0d9fd73387d040cab05dfa69
Nathan4043 <nathanr88@nycstudents.net>
2025-11-19 10:10:48 -0500

    Update guardrail.py

--- guardrail.py
+++ guardrail.py
@@ -1,12 +1,19 @@
 def password(email, password):
+    if "" not in password:
+        print("need to be string")
+        return
     if "@" not in email:
         print("missing a @!")
         return  
     if len(password)<8:
         print("password too short!(8)")
         return
-    if password == password.lower:
-        print("must use cap letter")
-    print("ok email and pass good")
+    if not any(char.isdigit() for char in password):
+        print("password needs at least 1 number")
+        return
+    if not any (char.isupper ()for char in password):
+        print("must contain upper case")
+        return
+    print("ok email and pass good account is good")
 
-print(password("test@gmail.com", "testoooooo"))
+print(password("test@gmail.com", "teFstx2fhgf"))
