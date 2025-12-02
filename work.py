20ee5966642738fd5ac605d3e1d5986f48162f38
Nathan4043 <nathanr88@nycstudents.net>
2025-12-02 10:11:04 -0500

    Update work.py

--- work.py
+++ work.py
@@ -11,9 +11,9 @@ slot=random.randint(1,10)
 Pet = pet("pet", 50, 50,50,50,100)
 print (Pet. __dict__)
 while True:
+    options = ["1","2","3","4","5"]
     activity= input ("1:play or 2:feed or 3:water or 4:care 5:slot machine")
-
-    if activity.lower() == "1":
+    def hungry():
         Pet.hunger += 10
         Pet.happiness -=2
         Pet.love -=2
@@ -25,7 +25,7 @@ while True:
         print("pet was unfed and died")
         exit()
 
-    if activity.lower() == "2":
+    def happiness():
         Pet.happiness += 10
         Pet.hunger -=2
         Pet.love -=2
@@ -37,7 +37,7 @@ while True:
         print("pet was despressed he lowkey might be dead")
         exit()
 
-    if activity.lower() == "3":
+    def water():
         Pet.thirst += 10
         Pet.hunger -=2
         Pet.love -=2
@@ -48,8 +48,8 @@ while True:
     if Pet.thirst <=0:
         print("pet was not given water, pet is dried up like a raisin")
         exit()
-
-    if activity.lower() == "4":
+    
+    def love():
         Pet.love += 10
         Pet.happiness -=2
         Pet.hunger-=2
@@ -60,11 +60,21 @@ while True:
     if Pet.love <=0:
         print("pet was uncared for, it ran away")
         exit()
-    print(f"Hunger: {Pet.hunger}")
-    print(f"Happiness: {Pet.happiness}")
-    print(f"Thirst: {Pet.thirst}")
-    print(f"Love: {Pet.love}")
-    print(f"Money$:{Pet.money}")
+
+    if activity.lower() == "1":
+        hungry()
+
+    elif activity.lower() == "2":
+        happiness()
+
+    elif activity.lower() == "3":
+        water()
+
+    elif activity.lower() == "4":
+        love()
+    elif activity not in options:
+        print("Wrong option, Choose again")
+        continue
 
     if Pet.hunger > 100:
         Pet.hunger = 100
@@ -79,14 +89,18 @@ while True:
         print("dawg, you in cripping debt")
         break
 
-
-
     if activity.lower() == "5":
-        slot=random.randint(1,50)
+        slot=random.randint(7,77)
         Pet.money -=5
-        if slot == 7:
+        if slot == 77:
             print("YOU WON BIG KEEP GOING DUDE")
-            Pet.money += 250
+            Pet.money += 777
+
+    print(f"Hunger: {Pet.hunger}")
+    print(f"Happiness: {Pet.happiness}")
+    print(f"Thirst: {Pet.thirst}")
+    print(f"Love: {Pet.love}")
+    print(f"Money$:{Pet.money}")
         
     
       
