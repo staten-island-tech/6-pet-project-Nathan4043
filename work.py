8f16aee5e1d6d33873af8b5fc0a4efedf1ecffb0
Nathan4043 <nathanr88@nycstudents.net>
2025-12-09 10:15:01 -0500

    Update work.py

--- work.py
+++ work.py
@@ -9,61 +9,62 @@ class pet:
         self.love = int(love)
         self.money = int(money)
 slot=random.randint(1,10)
-Pet = pet(name_pet, 50, 50,50,50,100)
-petstat= [Pet.hunger, Pet.thirst, Pet.happiness, Pet.love, Pet.money]
-print (Pet. __dict__)
+self=pet(name_pet, 50, 50,50,50,100)
+self = pet(name_pet)
+petstat= [self.hunger, self.thirst, self.happiness, self.love, self.money]
+print (self. __dict__)
 while True:
     options = ["1","2","3","4","5"]
     activity= input ("1:play or 2:feed or 3:water or 4:care 5:slot machine")
     def hungry():
-        Pet.hunger += 10
-        Pet.happiness -=2
-        Pet.love -=2
-        Pet.thirst -=2
-        Pet.money += 10
+        self.hunger += 10
+        self.happiness -=2
+        self.love -=2
+        self.thirst -=2
+        self.money += 10
 
     def happiness():
-        Pet.happiness += 10
-        Pet.hunger -=2
-        Pet.love -=2
-        Pet.thirst -=2
-        Pet.money += 10
+        self.happiness += 10
+        self.hunger -=2
+        self.love -=2
+        self.thirst -=2
+        self.money += 10
 
     def water():
-        Pet.thirst += 10
-        Pet.hunger -=2
-        Pet.love -=2
-        Pet.happiness-=2
-        Pet.money += 10
+        self.thirst += 10
+        self.hunger -=2
+        self.love -=2
+        self.happiness-=2
+        self.money += 10
 
     def love():
-        Pet.love += 10
-        Pet.happiness -=2
-        Pet.hunger-=2
-        Pet.money += 10
-        Pet.thirst -=2
+        self.love += 10
+        self.happiness -=2
+        self.hunger-=2
+        self.money += 10
+        self.thirst -=2
 
     def overcap():
         for stat in ("hunger", "love", "thirst", "happiness"):
-            setattr(Pet, stat, min(getattr(Pet, stat), 100))
-            #condenses the like the 40 if statements better
+            setattr(self, stat, min(getattr(self, stat), 100))
+            
     def gamble():
         slot=random.randint(7,77)
-        Pet.money -=7
+        self.money -=7
         if slot == 77:
             print("YOU WON BIG KEEP GOING DUDE")
-            Pet.money += 777
+            self.money += 777
 
     def checkdeath():
-        if Pet.hunger <=0:
+        if self.hunger <=0:
             print (f"{name_pet} was not fed, it died")
-        elif Pet.love <=0:
+        elif self.love <=0:
             print(f"{name_pet} was uncared for, it ran away")
             exit()
-        elif Pet.happiness <=0:
+        elif self.happiness <=0:
             print(f"{name_pet} was despressed it lowkey might be dead")
             exit()
-        elif Pet.thirst <=0:
+        elif self.thirst <=0:
             print(f"{name_pet} was not given water, pet is dried up like a raisin")
             exit()
         elif activity.lower() == "5":
@@ -86,7 +87,7 @@ while True:
     overcap()
     checkdeath()
 
-    print(f"{name_pet}'s stats: Happiness:{Pet.happiness} Hunger:{Pet.hunger} Thirst:{Pet.thirst} Love:{Pet.love} Cash:{Pet.money}")
+    print(f"{name_pet}'s stats: Happiness:{self.happiness} Hunger:{self.hunger} Thirst:{self.thirst} Love:{self.love} Cash:{self.money}")
 
 
 
